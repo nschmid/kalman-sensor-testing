4408f41aed1ee77951835119bd6c6db293ad5f36
Nicolas Schmid <nicolasschmid@192.168.1.5>
2016-01-04 12:53:07 +0100

    more stuff

--- src/ch/bfh/mobileroboter/KalmanTestsController.java
+++ src/ch/bfh/mobileroboter/KalmanTestsController.java
@@ -1,5 +1,58 @@
 package ch.bfh.mobileroboter;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import pk.com.habsoft.robosim.filters.kalman.KalmanFilterSimulator;
+
 public class KalmanTestsController {
+
+	final static String TOTAL_TIME_TAG = "TOTAL_TIME";
+	final static String VARIANCE_TAG = "VARIANCE";
+	final static String CAR_SPEED_TAG = "CAR_SPEED";
+
+	static int DEFAULT_TOTAL_TIME = 20;
+	final static int MIN_TOTAL_TIME = 10;
+	final static int MAX_TOTAL_TIME = 100;
+
+	static double DEFAULT_VARIANCE = 3;
+	final static int MIN_VARIANCE = 0;
+	final static int MAX_VARIANCE = 10;
+
+	static double DEFAULT_CAR_SPEED = 0.5;
+
+	// LineChartPanel pnlPosition = null;
+
+	// BarChartPanel pnlPositionError = null;
+
+	@FXML
+	private Button btnUpdate;
 	
+	@FXML
+	private Spinner<Double> spnTotalTime = new Spinner<>();
+	
+	@FXML
+	private Spinner<Double> spnVariance = new Spinner<>();
+	
+	@FXML
+	private Spinner<Double> spnRobotSpeed = new Spinner<>();
+
+	KalmanFilterSimulator simulator = null;
+
+	private void update() {
+		DEFAULT_TOTAL_TIME = Integer.parseInt(spnTotalTime.getValue().toString());
+		DEFAULT_VARIANCE = Double.parseDouble(spnVariance.getValue().toString());
+		DEFAULT_CAR_SPEED = Double.parseDouble(spnRobotSpeed.getValue().toString());
+		update(DEFAULT_TOTAL_TIME, DEFAULT_VARIANCE, DEFAULT_CAR_SPEED);
+	}
+
+	public void update(int total_time, double variance, double carSpeed) {
+		simulator = new KalmanFilterSimulator(total_time, variance, carSpeed);
+		simulator.simulate();
+//		pnlPosition.clearData();
+//		pnlPosition.addData("Measurement", simulator.getPositionMeasurements());
+//		pnlPosition.addData("Car Position", simulator.getCarPositions());
+//		pnlPosition.addData("Kalman Postion", simulator.getPositionsKalman());
+
+//		pnlPositionError.setData(simulator.getPositionMeasurementError(), simulator.getPositionKalmanError());
+	}
 }
